1a9580ff7586cb343de028807bf03476819d062e
tomfriwel <976932447@qq.com>
2018-09-05 18:03:13 +0800

    filter

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,111 @@
 // pages/index/index.js
+const ImageFilters = require('../../utils/weImageFilters/weImageFilters.js')
+
+const canvasId = 'hehe'
+const canvasW = 300
+const canvasH = 300
+
+let originalData = null
+
+const filters = {
+    original: function (data) {
+        return data
+    },
+    black: function (data) { },
+    gray: function (data) { }
+}
+
+const keys = Object.keys(filters)
+
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
-
+    onLoad: function (options) {
     },
+    filterTap() {
+        wx.showLoading({
+            title: '正在加载...',
+        })
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
+        let imageData = ImageFilters.utils.createImageDataFromData(originalData, canvasW, canvasH)
+        // new ImageData(originalData, canvasW, canvasH)
 
+        // let filtered = ImageFilters.GrayScale(imageData)
+        // let filtered = ImageFilters.Mosaic(imageData, 10)
+        let filtered = ImageFilters.GaussianBlur(imageData, 4)
+
+        wx.canvasPutImageData({
+            canvasId: canvasId,
+            data: filtered.data,
+            x: 0,
+            y: 0,
+            width: canvasW,
+            height: canvasH,
+            complete: res => {
+                console.log(res)
+                wx.hideLoading()
+            }
+        })
+        return
+        const z = this
+        wx.showActionSheet({
+            itemList: keys,
+            success: res => {
+                let {
+                    tapIndex
+                } = res
+
+                let data = new Uint8ClampedArray(originalData)
+                data = filters[keys[tapIndex]](data)
+
+                wx.canvasPutImageData({
+                    canvasId: canvasId,
+                    data: data,
+                    x: 0,
+                    y: 0,
+                    width: canvasW,
+                    height: canvasH,
+                    complete: res => {
+                        console.log(res)
+                    }
+                })
+            }
+        })
     },
+    choose() {
+        const z = this
+        const ctx = wx.createCanvasContext(canvasId)
+        wx.chooseImage({
+            count: 1,
+            success: function (res) {
+                if (res.tempFilePaths.length) {
+                    let path = res.tempFilePaths[0]
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function() {
-
+                    ctx.drawImage(path, 0, 0, canvasW, canvasH)
+                    ctx.draw(false, () => {
+                        console.log('draw done')
+                        z.getCavasImageData()
+                    })
+                }
+            },
+        })
     },
+    getCavasImageData() {
+        const z = this
+        wx.canvasGetImageData({
+            canvasId: canvasId,
+            x: 0,
+            y: 0,
+            width: canvasW,
+            height: canvasH,
+            success: res => {
+                console.log(res)
+                let {
+                    data
+                } = res
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-
+                originalData = data
+            }
+        })
     }
 })
